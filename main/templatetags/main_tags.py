f0b4aecdbc05dcb32d2207b4811664fd8f5aa569
mohammed.karman <mohammed.karman@dwidasa.com>
2023-01-08 12:51:28 +0700

    fix statics

--- main/templatetags/main_tags.py
+++ main/templatetags/main_tags.py
@@ -1,4 +1,8 @@
+import os
+import uuid
+
 from django import template
+from django.conf import settings
 from django.template.context import RenderContext
 from django.forms import ModelForm
 
@@ -27,3 +31,14 @@ def isVarExists(context: RenderContext, name: str) -> bool:
             if name in d:
                 return True
     return False
+
+
+@register.simple_tag(name='cache_bust')
+def cache_bust():
+
+    if settings.DEBUG:
+        version = uuid.uuid1()
+    else:
+        version = settings.PROJECT_VERSION
+
+    return 'version={version}'.format(version=version)
