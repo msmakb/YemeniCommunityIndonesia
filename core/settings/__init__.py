724bd67239947df79a7e700b836444268f510bcf
mohammed.karman <mohammed.karman@dwidasa.com>
2024-05-26 20:50:15 +0700

    [Bug Fix] fix number of overdue months

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -9,7 +9,7 @@ from core.settings.mail import *
 from core.settings.urls import *
 
 # Version
-PROJECT_VERSION = '1.3.3'
+PROJECT_VERSION = '2.0.0'
 
 # Site Under Maintenance
 UNDER_MAINTENANCE = False
